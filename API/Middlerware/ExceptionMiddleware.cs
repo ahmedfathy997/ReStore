1aaf7446ae17b353d323321a6f39ad4f9ac7166d
Ahmed Fathy <Ahmed.Fathy@itworx.com>
2024-07-14 18:12:01 +0300

    Error Handling Setup

---
+++ API/Middlerware/ExceptionMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace API.Middlerware
+{
+    public class ExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionMiddleware> _logger;
+        private readonly IHostEnvironment _hostEnvironment;
+
+        public ExceptionMiddleware(RequestDelegate next,
+            ILogger<ExceptionMiddleware> logger, IHostEnvironment hostEnvironment)
+        {
+            _next = next;
+            _logger = logger;
+            _hostEnvironment = hostEnvironment;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, ex.Message);
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = 500;
+
+                var response = new ProblemDetails
+                {
+                    Status = 500,
+                    Detail = _hostEnvironment.IsDevelopment() ? ex.StackTrace?.ToString() : null,
+                    Title = ex.Message
+                };
+
+                var options = new JsonSerializerOptions
+                { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+
+                var json = JsonSerializer.Serialize(response, options);
+
+                await context.Response.WriteAsync(json);
+            }
+        }
+    }
+}
--- client/src/features/catalog/Catalog.tsx
+++ client/src/features/catalog/Catalog.tsx
@@ -1,18 +1,14 @@
 import { useState, useEffect } from "react";
-import { Product } from "../../app/models/product"
+import { Product } from "../../app/models/product";
 import ProductList from "./ProductList";
+import agent from "../../app/api/agent";
 
-
-export default function Catalog(){
+export default function Catalog() {
   const [products, setProducts] = useState<Product[]>([]);
 
   useEffect(() => {
-    fetch('http://localhost:5225/api/product')
-    .then(response => response.json())
-    .then(data => setProducts(data))
-  }, [])
+    agent.Catalog.list().then((products) => setProducts(products));
+  }, []);
 
-    return(
-      <ProductList products={products} />
-    )
-  }
+  return <ProductList products={products} />;
+}
